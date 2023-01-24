dd43cce1cfab1581d5edf45628507d6277c5f7b5
Amalia Oktaviana <amalia.okv01@gmail.com>
2023-01-24 15:10:29 +0100

    Add Restaurant Image utils in PictureExt.kt

--- app/src/main/java/com/ameliok/newvenues/PictureExt.kt
+++ app/src/main/java/com/ameliok/newvenues/PictureExt.kt
@@ -2,5 +2,15 @@ package com.ameliok.newvenues
 
 import android.view.View
 import android.widget.ImageView
+import com.bumptech.glide.Glide
 
-fun ImageView.setImageUrl
+fun ImageView.setImageUrl(imgUrl: String?) {
+    if (imgUrl.isNullOrBlank()) return
+
+    Glide.with(this)
+        .load(imgUrl)
+        .into(this)
+}
+
+fun ImageView.setRestaurantIconUrl(iconCode: String?) =
+    setImageUrl("https://prod-wolt-venue-images-cdn.wolt.com/$iconCode.jpg")
