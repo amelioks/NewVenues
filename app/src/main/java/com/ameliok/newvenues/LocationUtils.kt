35fbdfe51371ac0bdf5de52de78f439a5bbb20ca
Amalia Oktaviana <amalia.okv01@gmail.com>
2023-01-25 21:40:31 +0100

    Adding location permission to check restaurant current user location

---
+++ app/src/main/java/com/ameliok/newvenues/LocationUtils.kt
@@ -0,0 +1,32 @@
+package com.ameliok.newvenues
+
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+
+val runningQOrLater = android.os.Build.VERSION.SDK_INT >=
+        android.os.Build.VERSION_CODES.Q
+
+
+
+//Check if fine background location permission granted
+@TargetApi(29)
+fun Context.fineAndCoarseLocationPermissionGranted(): Boolean {
+    val fineLocationGranted = ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    val coarseLocationGranted =
+        if (runningQOrLater) {
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
+    return fineLocationGranted && coarseLocationGranted
+}
--- app/src/main/java/com/ameliok/newvenues/MainFragment.kt
+++ app/src/main/java/com/ameliok/newvenues/MainFragment.kt
@@ -1,16 +1,23 @@
 package com.ameliok.newvenues
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ameliok.newvenues.databinding.FragmentMainScreenBinding
 import com.ameliok.newvenues.service.ServiceBuilder
 import com.ameliok.newvenues.service.WoltVenueService
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 
 class MainFragment : Fragment() {
@@ -24,6 +31,8 @@ class MainFragment : Fragment() {
     private val adapter = GetRestaurantAdapter()
     private var _binding: FragmentMainScreenBinding? = null
     private val binding get() = _binding!!
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val REQUEST_LOCATION = 1
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,8 +58,64 @@ class MainFragment : Fragment() {
     private fun setupView(): View {
         binding.restaurantRecycler.adapter = adapter
         binding.restaurantRecycler.layoutManager = LinearLayoutManager(context)
+        enableUserLocation()
         return binding.root
     }
+
+    // Checks if users have given their location and sets location enabled if so.
+    @SuppressLint("MissingPermission")
+    fun enableUserLocation() {
+        if (requireContext().fineAndCoarseLocationPermissionGranted()) {
+
+        } else {
+            requestPermissions(
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION
+            )
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getRestaurantLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    viewModel.getRestaurantCurrentLocationData(
+                        location.latitude,
+                        location.longitude
+                    )
+                } else {
+                    Toast.makeText(context, "Location not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // permission was granted.
+                    getRestaurantLocation()
+                } else {
+                    // permission denied.
+                    // tell the user the action is cancelled
+                    Toast.makeText(context, "Permission is not granted!", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
