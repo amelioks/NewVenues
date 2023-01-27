b92708b81317f5285072d6a9899615563232e5ac
Amalia Oktaviana <amalia.okv01@gmail.com>
2023-01-27 11:52:53 +0100

    Adding SharedPreferenceHelper.kt to save favorite state

--- app/src/main/java/com/ameliok/newvenues/data/SharedPreferenceHelper.kt
+++ app/src/main/java/com/ameliok/newvenues/data/SharedPreferenceHelper.kt
@@ -11,6 +11,14 @@ class SharedPreferenceHelper(private val context: Context) {
         Context.MODE_PRIVATE
     )
 
+    var favoriteVenuesIds : Set<String>
+        get() = sharedPreferences.getStringSet(KEY_STATE, emptySet()) as Set<String>
+        set(value) {
+            sharedPreferences.edit().putStringSet(KEY_STATE,value).apply()
+        }
 
+    companion object {
+        const val KEY_STATE = "pref_key_last_state"
+    }
 
 }
