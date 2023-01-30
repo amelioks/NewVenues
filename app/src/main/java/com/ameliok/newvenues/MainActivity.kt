1ddfa13c3b4109367aa5877c878af6c668bada3c
Amalia Oktaviana <amalia.okv01@gmail.com>
2023-01-30 22:46:35 +0100

    delete double fragment container

--- app/src/main/java/com/ameliok/newvenues/MainActivity.kt
+++ app/src/main/java/com/ameliok/newvenues/MainActivity.kt
@@ -1,20 +1,6 @@
 package com.ameliok.newvenues
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import com.ameliok.newvenues.ui.MainFragment
 
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<MainFragment>(R.id.fragmentContainerView)
-            }
-        }
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
