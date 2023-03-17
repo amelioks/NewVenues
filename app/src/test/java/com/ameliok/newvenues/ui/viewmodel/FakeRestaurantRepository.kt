8bfa070209c1fd27660dda9afaf6799e547f241a
Amalia Oktaviana <amalia.okv01@gmail.com>
2023-03-18 00:25:33 +0100

    Adding FakeRestaurantRepository.kt

--- app/src/test/java/com/ameliok/newvenues/ui/viewmodel/FakeRestaurantRepository.kt
+++ app/src/test/java/com/ameliok/newvenues/ui/viewmodel/FakeRestaurantRepository.kt
@@ -1,24 +1,57 @@
 package com.ameliok.newvenues.ui.viewmodel
 
+import com.ameliok.newvenues.data.api.model.Venue
+import com.ameliok.newvenues.domain.model.ImageDomain
 import com.ameliok.newvenues.domain.model.ItemDomain
+import com.ameliok.newvenues.domain.model.VenueDomain
 import com.ameliok.newvenues.domain.repository.RestaurantRepository
 
-class FakeRestaurantRepository: RestaurantRepository {
-    val fakeData = listOf<ItemDomain>()
+class FakeRestaurantRepository : RestaurantRepository {
+    private val fakeRestaurantList = listOf(
+        ItemDomain(
+            ImageDomain("https://example.com/image1.jpg"),
+            VenueDomain(
+                "restaurant1",
+                "restaurantABC",
+                "asian"
+            ),
+            true
+        ),
+        ItemDomain(
+            ImageDomain("https://example.com/image2.jpg"),
+            VenueDomain(
+                "restaurant2",
+                "restaurantDEF",
+                "american"
+            ),
+            false
+        ),
+        ItemDomain(
+            ImageDomain("https://example.com/image3.jpg"),
+            VenueDomain(
+                "restaurant3",
+                "restaurantGHI",
+                "africam"
+            ),
+            true
+        )
+    )
+
+    private val favoriteRestaurant = mutableSetOf<String>()
 
     override suspend fun getRestaurant(lat: Double, lon: Double): List<ItemDomain> {
-        TODO("Not yet implemented")
+        return fakeRestaurantList
     }
 
     override fun saveFavoriteRestaurant(id: String) {
-        TODO("Not yet implemented")
+        favoriteRestaurant.add(id)
     }
 
     override fun removeFavoriteRestaurant(id: String) {
-        TODO("Not yet implemented")
+        favoriteRestaurant.remove(id)
     }
 
     override fun isFavoriteRestaurant(id: String): Boolean {
-        TODO("Not yet implemented")
+        return favoriteRestaurant.contains(id)
     }
 }
